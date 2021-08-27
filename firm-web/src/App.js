cacf370579356d47a82b9c08bb5f06ac0bb3fdb5
Minh Tran <sater@pop-os.localdomain>
2021-08-28 00:07:33 +0700

    firm web

---
+++ firm-web/src/App.js
@@ -0,0 +1,11 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+import {BroswerRouter as Router, Route} from 'react-router'
+
+function App(){
+    return(
+        <div>
+
+        </div>
+    )
+}
