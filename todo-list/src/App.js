8f1924f3f7d823ec7a1ecb17847689e134afdcc3
Minh Tran <sater@pop-os.localdomain>
2021-08-29 00:28:29 +0700

    todo list

---
+++ todo-list/src/App.js
@@ -0,0 +1,16 @@
+import React from  'react'
+import Time from './Time'
+import Form from './Form'
+import List from './List'
+
+function App(){
+    return(
+        <div>
+            <Time></Time>
+            <Form></Form>
+            <List></List>
+
+        </div>
+    )
+}
+export default App;
