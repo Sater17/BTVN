7c7d0e70d8e1398579d017fa6bdea6081e70ab47
Minh Tran <sater@pop-os.localdomain>
2021-09-05 21:45:59 +0700

    api book

--- api-book/index.js
+++ api-book/index.js
@@ -1,53 +1,78 @@
-const express=require('express')
+const express = require('express')
 const app = express()
-const cors=require('cors')
-const helmet=require('helmet')
-const fs =require('fs')
+const cors = require('cors')
+const helmet = require('helmet')
+const fs = require('fs')
 const os = require('os')
+const path = require('path')
 
 app.use(express.json())
 app.use(express.static('logger'))
+app.set('trust proxy', true)
 
-const data={
-    1:{ 
+const data = {
+    1: {
         name: 'Deadpool',
         genre: 'Humor, Literature'
     },
-    2:{ 
+    2: {
         name: 'Superman',
         genre: 'Action, Love Story, Romantic'
     }
 }
+const key = Object.keys(data)
+
+
+//function middle
 
 function bookApi(req, res, next) {
-    fs.appendFile('log.txt',req.ip+'\n',(e)=>{
-       if(e){ console.log('e')}
-        else{console.log('no e')}
-    })
-    res.send('asd')
+    fs.appendFile(__dirname + '/logger/log.txt', req.ip +
+        req.method + req.originalUrl + '\n', (e) => {
+            if (e) { console.log('e') }
+           
+        })
     next()
 }
-// app.use('/books',(req,res,next) => {
-//     // fs.appendFile('log.txt',req.ip+'\n')
-//     res.send('sdsa')
-//     next();
-// })
-app.get('/',bookApi,(req,res)=>{
-    res.send('sucessfully')
+function isValid(req, res, next) {
+    if (req.params.id > key.length) {
+        res.sendStatus(404)
+    }
+    next();
+}
+
+//middleware
+app.use('/books', (req, res, next) => {
+    fs.appendFile(__dirname + '/logger/log.txt', 'ip: ' + req.ip +
+        ' method: ' + req.method + ' URL: ' + req.originalUrl + ' path: ' + req.path + '\n', (e) => {
+            if (e) { console.log('e') }
+            
+        })
+    next();
 })
-app.get('/books',(req,res)=>{
+
+
+//METHOD
+app.get('/', bookApi, (req, res) => {
+    res.sendFile(path.join(__dirname, 'index.html'));
+})
+app.get('/books', (req, res) => {
     res.json(data)
 })
-app.get('/books/:id',(req,res)=>{
+app.get('/books/:id',isValid, (req, res) => {
     res.json(data[req.params.id])
 })
 
-app.post('/books',(req,res)=>{
+app.post('/books', (req, res) => {
+
     console.log('post success')
 })
-app.delete('/books/:id',(req,res)=>{
-    data[req.params.id]=undefined;
+app.delete('/books/:id', (req, res) => {
+    data[req.params.id] = undefined;
     console.log('delete success')
+})
+app.put('/books/:id',(req, res)=>{
+    data[req.params.id]=req.body
+    console.log('put success')
 })
 
 app.listen(8080)
